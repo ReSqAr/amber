da6d3b4ac849ed58bf57ca2bd646ff675a297ce6
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-01-20 17:09:58 +0000

    moved code around

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -6,23 +6,25 @@ use crate::repository::local_repository::LocalRepository;
 use crate::repository::logic::sync;
 use crate::repository::traits::{LastIndicesSyncer, Metadata};
 use anyhow::Result;
-use log::{debug, info};
+use log::debug;
 
 pub async fn sync(port: u16) -> Result<(), Box<dyn std::error::Error>> {
     let local = LocalRepository::new(None).await?;
     let remote = GRPCClient::connect(format!("http://127.0.0.1:{}", port)).await?;
 
     let local_repo_id = local.repo_id().await?;
-    debug!("local repo_id={}", local_repo_id);
-
     let remote_repo = remote.repo_id().await?;
-    info!("remote repo_id={}", remote_repo);
+    debug!(
+        "repo_id  - local: {} remote: {}",
+        local_repo_id, remote_repo
+    );
 
     let remote_last_indices = remote.lookup(local_repo_id).await?;
-    debug!("remote remote_last_indices={:?}", remote_last_indices);
-
     let local_last_indices = local.lookup(remote_repo).await?;
-    debug!("local local_last_indices={:?}", local_last_indices);
+    debug!(
+        "remote_last_indices - local: {:?} remote: {:?}",
+        local_last_indices, remote_last_indices
+    );
 
     sync::sync_table::<DbFile, _, _>(
         local.clone(),
