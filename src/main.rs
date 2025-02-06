73b31c5ab2725ddfb42dd055a625c7ff401b20a2
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-02-06 19:31:08 +0000

    removed main duration log

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,9 @@ mod grpc;
 mod repository;
 mod utils;
 
-use tokio::time::Instant;
-
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    let start_time = Instant::now();
-
     cli::run().await;
-    let duration = start_time.elapsed();
-    println!("took: {:.2?}", duration);
 }
