3c22c3a8ba5110d7eb601ca4cdf483c63dae6aed
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-02-06 19:31:53 +0000

    flightdeck: file writer

---
+++ src/flightdeck/file_manager.rs
@@ -0,0 +1,75 @@
+use crate::flightdeck::observation::Observation;
+use crate::flightdeck::observation::Value;
+use chrono::SecondsFormat;
+use sqlx::types::JsonValue;
+use tokio::io::{AsyncWrite, AsyncWriteExt};
+
+pub struct FileManager {
+    writer: Box<dyn AsyncWrite + Send + Sync + Unpin>,
+    level_filter: log::LevelFilter,
+}
+
+impl FileManager {
+    pub fn new(
+        writer: Box<dyn AsyncWrite + Send + Sync + Unpin>,
+        level_filter: log::LevelFilter,
+    ) -> Self {
+        Self {
+            writer,
+            level_filter,
+        }
+    }
+}
+
+impl FileManager {
+    pub(crate) async fn observe(&mut self, level: log::Level, obs: Observation) {
+        if level >= self.level_filter {
+            return;
+        }
+
+        let mut log_object = serde_json::Map::new();
+
+        log_object.insert("level".to_string(), JsonValue::String(level.to_string()));
+
+        if let Some(id_value) = obs.id.map(JsonValue::String) {
+            log_object.insert("id".to_string(), id_value);
+        }
+
+        let timestamp = obs.timestamp.to_rfc3339_opts(SecondsFormat::Millis, true);
+        log_object.insert("timestamp".to_string(), JsonValue::String(timestamp));
+
+        for data in obs.data {
+            let value = match data.value {
+                Value::String(s) => JsonValue::String(s),
+                Value::U64(u) => JsonValue::Number(serde_json::Number::from(u)),
+            };
+            log_object.insert(data.key, value);
+        }
+
+        // serialise
+        let json_line = match serde_json::to_string(&log_object) {
+            Ok(s) => s,
+            Err(e) => {
+                log::error!("flightdeck error: unable to serialise log message: {}", e);
+                return;
+            }
+        };
+
+        // Write the JSON line followed by a newline.
+        if let Err(e) = self.writer.write_all(json_line.as_bytes()).await {
+            log::error!("flightdeck error: unable to write logs: {}", e);
+        }
+        if let Err(e) = self.writer.write_all(b"\n").await {
+            log::error!("flightdeck error: unable to write logs: {}", e);
+        }
+        if let Err(e) = self.writer.flush().await {
+            log::error!("flightdeck error: unable to flush logs: {}", e);
+        }
+    }
+
+    pub(crate) async fn finish(&mut self) {
+        if let Err(e) = self.writer.flush().await {
+            log::error!("flightdeck error: unable to flush logs: {}", e);
+        }
+    }
+}
--- src/flightdeck/mod.rs
+++ src/flightdeck/mod.rs
@@ -1,19 +1,59 @@
+use crate::flightdeck::file_manager::FileManager;
 use crate::flightdeck::global::GLOBAL_LOGGER;
 use crate::flightdeck::observation::Message;
 use crate::flightdeck::observation::Observation;
 use crate::flightdeck::progress_manager::{LayoutItemBuilderNode, ProgressManager};
+use std::path::{Path, PathBuf};
+use tokio::fs;
 use tokio::sync::broadcast;
 
 pub mod base;
+pub mod file_manager;
 pub mod global;
 pub mod layout;
 pub mod observation;
 pub mod observer;
 pub mod progress_manager;
 
-pub trait Manager: Send + Sync {
-    fn observe(&mut self, level: log::Level, obs: Observation);
-    fn finish(&self);
+#[derive(Default)]
+struct Manager {
+    progress_manager: Option<ProgressManager>,
+    file_manager: Option<FileManager>,
+}
+
+impl Manager {
+    pub(crate) async fn observe(&mut self, level: log::Level, obs: Observation) {
+        if let Some(progress_manager) = self.progress_manager.as_mut() {
+            progress_manager.observe(level, obs.clone()).await;
+        }
+        if let Some(file_manager) = self.file_manager.as_mut() {
+            file_manager.observe(level, obs.clone()).await;
+        }
+    }
+
+    pub(crate) async fn finish(&mut self) {
+        if let Some(progress_manager) = &self.progress_manager {
+            progress_manager.finish().await;
+        }
+        if let Some(file_manager) = self.file_manager.as_mut() {
+            file_manager.finish().await;
+        }
+    }
+}
+
+impl Manager {
+    pub(crate) fn set_progress(self, progress_manager: ProgressManager) -> Self {
+        Self {
+            progress_manager: Some(progress_manager),
+            file_manager: self.file_manager,
+        }
+    }
+    pub(crate) fn set_file(self, file_manager: FileManager) -> Self {
+        Self {
+            progress_manager: self.progress_manager,
+            file_manager: Some(file_manager),
+        }
+    }
 }
 
 pub struct NotifyOnDrop {
@@ -40,17 +80,21 @@ pub fn notify_on_drop() -> (NotifyOnDrop, broadcast::Receiver<()>) {
 
 #[derive(Default)]
 pub struct FlightDeck {
-    manager: Vec<Box<dyn Manager>>,
+    manager: Manager,
 }
 
 pub async fn flightdeck<E: From<tokio::task::JoinError>>(
     wrapped: impl std::future::Future<Output = Result<(), E>> + Sized,
     root_builders: impl IntoIterator<Item = LayoutItemBuilderNode> + Sized + Send + Sync + 'static,
+    path: PathBuf,
+    level_filter: log::LevelFilter,
 ) -> Result<(), E> {
     let (drop_to_notify, notify) = notify_on_drop();
-    let join_handle = tokio::spawn(async {
+    let join_handle = tokio::spawn(async move {
         FlightDeck::new()
             .with_progress(root_builders)
+            .with_file(path, level_filter)
+            .await
             .run(notify)
             .await;
     });
@@ -68,42 +112,59 @@ impl FlightDeck {
         Self::default()
     }
 
-    pub fn with_progress<I>(&mut self, root_builders: I) -> &mut Self
+    pub fn with_progress<I>(self, root_builders: I) -> Self
     where
         I: IntoIterator<Item = LayoutItemBuilderNode>,
     {
-        self.manager
-            .push(Box::new(ProgressManager::new(root_builders)));
-        self
+        Self {
+            manager: self
+                .manager
+                .set_progress(ProgressManager::new(root_builders)),
+        }
     }
 
-    pub async fn run(&mut self, mut notify: broadcast::Receiver<()>) {
+    pub async fn with_file(self, path: impl AsRef<Path>, level_filter: log::LevelFilter) -> Self {
+        let path = path.as_ref();
+
+        if let Some(parent) = path.parent() {
+            fs::create_dir_all(parent).await.unwrap_or_else(|_| {
+                panic!("unable to create parent directory for {}", path.display())
+            });
+        }
+
+        let file = fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(path)
+            .await
+            .unwrap_or_else(|_| panic!("unable to open log file {}", path.display()));
+        let writer = Box::new(file);
+        Self {
+            manager: self
+                .manager
+                .set_file(FileManager::new(writer, level_filter)),
+        }
+    }
+
+    pub async fn run(&mut self, mut shutdown: broadcast::Receiver<()>) {
         let mut rx_guard = GLOBAL_LOGGER.rx.lock().await;
 
         loop {
             tokio::select! {
-                _ = notify.recv() => {
+                _ = shutdown.recv() => {
                     while let Ok(Message { level, observation }) = rx_guard.try_recv() {
-                            for manager in self.manager.iter_mut() {
-                                manager.observe(level, observation.clone());
-                        }
+                        self.manager.observe(level, observation.clone()).await;
                     }
 
-                    for manager in self.manager.iter() {
-                        manager.finish();
-                    }
+                    self.manager.finish().await;
                     break;
                 },
 
                 msg = rx_guard.recv() => {
                     if let Some(Message { level, observation }) = msg {
-                        for manager in self.manager.iter_mut() {
-                            manager.observe(level, observation.clone());
-                        }
+                        self.manager.observe(level, observation.clone()).await;
                     } else {
-                        for manager in self.manager.iter() {
-                            manager.finish();
-                        }
+                        self.manager.finish().await;
                         break;
                     }
                 },
