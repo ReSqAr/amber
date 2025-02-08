ab94879f5968504bf141c5079654ca08280d6695
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-02-08 14:39:35 +0000

    Update mod.rs

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,12 +2,14 @@ pub(crate) mod database;
 pub(crate) mod migrations;
 pub(crate) mod models;
 
-use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions, SqliteJournalMode};
 use sqlx::{Error, SqlitePool};
 use std::str::FromStr;
 
 pub async fn establish_connection(database_url: &str) -> Result<SqlitePool, Error> {
-    let options = SqliteConnectOptions::from_str(database_url)?.create_if_missing(true);
+    let options = SqliteConnectOptions::from_str(database_url)?
+        .create_if_missing(true)
+        .journal_mode(SqliteJournalMode::Wal);
     SqlitePoolOptions::new()
         .max_connections(5) // Adjust based on your needs
         .connect_with(options)
