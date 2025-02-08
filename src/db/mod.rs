cf15a0d7bb96ae6157242b2e65c11f9a5f0f84ba
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-02-08 14:43:08 +0000

    Update mod.rs

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -9,7 +9,9 @@ use std::str::FromStr;
 pub async fn establish_connection(database_url: &str) -> Result<SqlitePool, Error> {
     let options = SqliteConnectOptions::from_str(database_url)?
         .create_if_missing(true)
-        .journal_mode(SqliteJournalMode::Wal);
+        .journal_mode(SqliteJournalMode::Wal)
+        .log_slow_statements(log::LevelFilter::Off, std::time::Duration::from_secs(30))
+        .busy_timeout(std::time::Duration::from_secs(60)); // TODO: auto optimise at the end
     SqlitePoolOptions::new()
         .max_connections(5) // Adjust based on your needs
         .connect_with(options)
