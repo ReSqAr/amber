47f37262d3f2dbbd2ad1154e0a427adae865d4ae
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-02-17 00:30:01 +0000

    increased sqlite constants
    source: https://gist.github.com/phiresky/978d8e204f77feaa0ab5cca08d2d5b27

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -12,7 +12,14 @@ pub async fn establish_connection(database_url: &str) -> Result<SqlitePool, Erro
         .create_if_missing(true)
         .journal_mode(SqliteJournalMode::Wal)
         .log_slow_statements(log::LevelFilter::Off, std::time::Duration::from_secs(30))
-        .busy_timeout(std::time::Duration::from_secs(120));
+        .busy_timeout(std::time::Duration::from_secs(120))
+        .pragma("cache_size", "-1048576")
+        .pragma("temp_store", "MEMORY")
+        .pragma("synchronous", "NORMAL")
+        .pragma("mmap_size", "30000000000")
+        .pragma("page_size", "32768")
+        .optimize_on_close(true, 10000000);
+
     SqlitePoolOptions::new()
         .max_connections(5) // Adjust based on your needs
         .connect_with(options)
