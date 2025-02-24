dacf726aa6c09482fb8fe147357efa0817a6d775
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-02-24 23:08:49 +0000

    more rm tests

--- tests/integration_dsl.rs
+++ tests/integration_dsl.rs
@@ -1007,6 +1007,46 @@ async fn integration_test_rclone_repo_fsck() -> Result<(), anyhow::Error> {
 
 #[tokio::test(flavor = "multi_thread")]
 #[serial]
+async fn integration_test_rm() -> Result<(), anyhow::Error> {
+    let script = r#"
+        # when
+        @a amber init a
+        @a write_file test.txt "Hello world!"
+        @a amber add
+
+        # action
+        @a amber rm test.txt
+        assert_output_contains "deleted test.txt"
+
+        # then
+        @a assert_does_not_exist test.txt
+    "#;
+    run_dsl_script(script).await
+}
+
+#[tokio::test(flavor = "multi_thread")]
+#[serial]
+async fn integration_test_rm_soft() -> Result<(), anyhow::Error> {
+    let script = r#"
+        # when
+        @a amber init a
+        @a write_file test.txt "Hello world!"
+        @a amber add
+
+        # action
+        @a amber rm --soft test.txt
+        assert_output_contains "deleted [soft] test.txt"
+
+        # then
+        @a assert_exists test.txt
+        @a amber status
+        assert_output_contains "new test.txt"
+    "#;
+    run_dsl_script(script).await
+}
+
+#[tokio::test(flavor = "multi_thread")]
+#[serial]
 async fn integration_test_rm_not_existing_file() -> Result<(), anyhow::Error> {
     let script = r#"
         # when
