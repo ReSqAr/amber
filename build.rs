9205612509df7f5bb7d0ff46a1982daa517b1725
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-11-29 10:32:26 +0000

    move GRPC code generation out of the build process

--- build.rs
+++ build.rs
@@ -1,4 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/grpc.proto")?;
+    if std::env::var_os("GENERATE_GRPC").is_some() {
+        tonic_build::configure()
+            .build_client(true)
+            .build_server(true)
+            .compile_well_known_types(true)
+            .extern_path(".google.protobuf.Timestamp", "::prost_types::Timestamp")
+            .out_dir("src/grpc/generated")
+            .compile_protos(&["proto/grpc.proto"], &["proto"])?;
+    }
+
     Ok(())
 }
