07bc9f8a3475dea93bc61131e54bcd9ec2179163
Yasin Zähringer <yasin.zaehringer-github@yhjz.de>
2025-12-30 20:32:32 +0100

    one more test

--- tests/integration_remote_rclone.rs
+++ tests/integration_remote_rclone.rs
@@ -25,3 +25,29 @@ async fn integration_test_rclone_repo_pull_push() -> Result<(), anyhow::Error> {
     "#;
     dsl_definition::run_dsl_script(script).await
 }
+
+#[tokio::test(flavor = "multi_thread")]
+async fn integration_test_rclone_repo_pull_push_sync_via_exported_parquet_store()
+-> Result<(), anyhow::Error> {
+    let script = r#"
+        # when
+        @a amber init a
+        @a write_file test.txt "Hello store!"
+        @a amber add
+
+        @b amber init b
+
+        @a amber remote add b local $ROOT/b
+        @a amber remote add store rclone :local:/$ROOT/rclone
+        @b amber remote add store rclone :local:/$ROOT/rclone
+
+        # action
+        @a amber push store
+        @b amber pull store
+
+        # then
+        assert_equal a b
+        @b assert_exists test.txt "Hello store!"
+    "#;
+    dsl_definition::run_dsl_script(script).await
+}
